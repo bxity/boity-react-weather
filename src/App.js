e0b6b3251b3dc2a537e6f136c204bb3fffa21ae5
Boitumelo <boityy.js@gmail.com>
2023-10-23 11:27:46 +0200

    added footer section to html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>React Weather App</h1>
+      <footer>
+        This project was coded by Boitumelo Vilakazi and is{" "}
+        <a href="https://github.com/bxity/boity-react-weather" target="_blank">
+          open-sourced on GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
