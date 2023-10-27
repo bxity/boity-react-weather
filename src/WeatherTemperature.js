4674ea4492e38dc0843c3a5e818d7f35315cd67f
Boitumelo <boityy.js@gmail.com>
2023-10-27 14:10:02 +0200

    fixed conversion error

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,44 +1,42 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("fahrenheit");
-
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
+  const [unit, setUnit] = useState("celsius");
 
   function showFahrenheit(event) {
     event.preventDefault();
     setUnit("fahrenheit");
   }
 
-  function celsius() {
-    return (5 * (props.fahrenheit - 32)) / 9;
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
   }
 
-  if (unit === "fahrenheit") {
+  if (unit === "celsius") {
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.fahrenheit)}</span>
+        <span className="temperature">{Math.round(props.celsius)}</span>
         <span className="unit">
-          °F ⎮{" "}
-          <a href="/" onClick={showCelsius}>
-            °C
+          °C ⎮{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
           </a>
         </span>
       </div>
     );
   } else {
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(celsius())}</span>
-      <span className="unit">
-        <a href="/" onClick={showFahrenheit}>
-          °F
-        </a>{" "}
-        ⎮ °C
-      </span>
-    </div>;
+      let fahrenheit = (props.celsius * 9) / 5 + 32;
+      return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(fahrenheit)}</span>
+        <span className="unit">
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          ⎮ °F
+        </span>
+      </div>
+    );
   }
 }
