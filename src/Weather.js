baae8a60dfdc6095bf4f0c14aa042fb16df10d34
Boitumelo <boityy.js@gmail.com>
2023-10-28 09:54:06 +0200

    added basic structure for weather forecast

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [weatherData, setweatherData] = useState({ ready: false });
@@ -62,7 +63,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-
+        <WeatherForecast />
       </div>
     );
   } else {
